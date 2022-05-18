faa8002726f8b9d24173ceff6fff6384d032741a
axmiel <101211171+axmiel@users.noreply.github.com>
2022-05-18 11:04:11 +0100

    Update source.py

--- source.py
+++ source.py
@@ -1,6 +1,13 @@
 fib_seq = [0,1]
 
-stop_loop = int(input("how many numbers frob fibonacci sequence should I generate" ))
-for i in range(stop_loop-2):
-    fib_seq.append(fib_seq[i] + fib_seq[i + 1])
-print(fib_seq)
+stop_loop = int(input("how many numbers frob fibonacci sequence should I generate: " ))
+
+if stop_loop == 0:
+    print("There are no numbers in the sequence")
+elif stop_loop == 1:
+    print(fib_seq[0])
+else:
+    for i in range(stop_loop-2):
+        next_number = (fib_seq[i] + fib_seq[i + 1])
+        fib_seq.append(next_number)
+        print(next_number)
